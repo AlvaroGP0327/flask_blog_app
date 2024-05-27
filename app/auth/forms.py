a38cc467ad72aec9221e902a9e60b88a9a5923f8
AlvaroGP <customerm552@gmail.com>
2024-05-27 12:01:36 -0400

    Version: Actualizacion de las hojas de estilo css.Pendiente Registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,8 +4,14 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email
 
 #Clase que representa formulario para logearse.
+#Se loggea un usuario que ya este registrado en la aplicacion
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Length(1,64),Email()])
     password = PasswordField('Password',validators=[DataRequired(),])
     remember_me = BooleanField('keep me logged in')
     submit = SubmitField('Log In')
+
+#Clase que representa el formulario de registro 
+#para usuarios nuevos.
+class RegistrationForm():
+    pass 
